9c8759d75435bd7b623c41ef064b748672676007
lochungtin <lochungtin@gmail.com>
2021-04-26 18:50:22 +0800

    initial commit with Rest server setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include "env.h"
+
+#define DEBUG
+WebServer server(80);
+
+// turn on all RGB components
+void lightsOn()
+{
+#ifdef DEBUG
+	Serial.println("Lights On");
+#endif
+
+	server.send(200, "OK");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// turn off all RGB components
+void lightsOff()
+{
+#ifdef DEBUG
+	Serial.println("Lights Off");
+#endif
+
+	server.send(200, "OK");
+}
+
+// initial setup procedure
+void setup()
+{
+// put your setup code here, to run once:
+#ifdef DEBUG
+	Serial.begin(9600);
+	Serial.println("Connecting to WiFi");
+#endif
+
+	// Connect to access point
+	WiFi.begin(ENV_SSID, ENV_PSWD);
+	while (WiFi.status() != WL_CONNECTED)
+		;
+
+#ifdef DEBUG
+	Serial.print("Connected - Local IP: ");
+	Serial.println(WiFi.localIP());
+#endif
+
+	// Rest API routing setup
+	server.on("/on", lightsOn);			// toggle on
+	server.on("/off", lightsOff);		// toggle off
+
+	server.begin();
+}
+
+// handle http client
+void loop()
+{
+	server.handleClient();
 }
