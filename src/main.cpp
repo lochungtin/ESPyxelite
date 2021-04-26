544eacad0f3edebb903a66c22c70f69bb98782ec
lochungtin <lochungtin@gmail.com>
2021-04-26 22:59:09 +0800

    routing setup

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,12 @@ void lightsOff()
 #endif
 }
 
-// new config
-void handleConfig()
+// new color
+void handleColor()
 {
 	server.send(200, "OK");
 #ifdef DEBUG
-	Serial.println("Set New Config");
+	Serial.println("Set New Color");
 #endif
 
 	// handle empty post requests
@@ -47,12 +47,132 @@ void handleConfig()
 		return;
 	}
 
-	String data = server.arg("plain");
-	deserializeJson(jsonDocument, data);
+	deserializeJson(jsonDocument, server.arg("plain"));
+	String color = jsonDocument["payload"];
+	jsonDocument.clear();
 
-	int test = jsonDocument["test"];
+#ifdef DEBUG
+	Serial.print("Received new color: ");
+	Serial.println(color);
+#endif
+}
 
-	Serial.println(test);
+// new action
+void handleAction()
+{
+	server.send(200, "OK");
+#ifdef DEBUG
+	Serial.println("Set New Action");
+#endif
+
+	// handle empty post requests
+	if (server.hasArg("plain") == false)
+	{
+#ifdef DEBUG
+		Serial.println("No JSON arg, no config set");
+#endif
+		return;
+	}
+
+	deserializeJson(jsonDocument, server.arg("plain"));
+	String action = jsonDocument["payload"];
+	jsonDocument.clear();
+
+#ifdef DEBUG
+	Serial.print("Received new action: ");
+	Serial.println(action);
+#endif
+}
+
+// new speed
+void handleSpeed()
+{
+	server.send(200, "OK");
+#ifdef DEBUG
+	Serial.println("Set New Speed");
+#endif
+
+	// handle empty post requests
+	if (server.hasArg("plain") == false)
+	{
+#ifdef DEBUG
+		Serial.println("No JSON arg, no config set");
+#endif
+		return;
+	}
+
+	deserializeJson(jsonDocument, server.arg("plain"));
+	int speed = jsonDocument["payload"];
+	jsonDocument.clear();
+
+#ifdef DEBUG
+	Serial.print("Received new speed: ");
+	Serial.println(speed);
+#endif
+}
+
+// new brightness
+void handleGamma()
+{
+	server.send(200, "OK");
+#ifdef DEBUG
+	Serial.println("Set New Brightness");
+#endif
+
+	// handle empty post requests
+	if (server.hasArg("plain") == false)
+	{
+#ifdef DEBUG
+		Serial.println("No JSON arg, no config set");
+#endif
+		return;
+	}
+
+	deserializeJson(jsonDocument, server.arg("plain"));
+	int gamma = jsonDocument["payload"];
+	jsonDocument.clear();
+
+#ifdef DEBUG
+	Serial.print("Received new brightness: ");
+	Serial.println(gamma);
+#endif
+}
+
+// new config
+void handleConfig()
+{
+	server.send(200, "OK");
+#ifdef DEBUG
+	Serial.println("Override Config");
+#endif
+
+	// handle empty post requests
+	if (server.hasArg("plain") == false)
+	{
+#ifdef DEBUG
+		Serial.println("No JSON arg, no config set");
+#endif
+		return;
+	}
+
+	deserializeJson(jsonDocument, server.arg("plain"));
+	String color = jsonDocument["color"];
+	String action = jsonDocument["action"];
+	int speed = jsonDocument["speed"];
+	int gamma = jsonDocument["gamma"];
+	jsonDocument.clear();
+
+#ifdef DEBUG
+	Serial.println("New Config Data:");
+	Serial.print("Color:\t");
+	Serial.println(color);
+	Serial.print("Action:\t");
+	Serial.println(action);
+	Serial.print("Speed:\t");
+	Serial.println(speed);
+	Serial.print("Brightness:\t");
+	Serial.println(gamma);
+#endif
 }
 
 // initial setup procedure
@@ -74,9 +194,13 @@ void setup()
 #endif
 
 	// Rest API routing setup
-	server.on("/on", lightsOn);	  // toggle on
-	server.on("/off", lightsOff); // toggle off
-	server.on("/config", HTTP_POST, handleConfig);
+	server.on("/on", lightsOn);					   // toggle on
+	server.on("/off", lightsOff);				   // toggle off
+	server.on("/color", HTTP_POST, handleColor);   // change color
+	server.on("/action", HTTP_POST, handleAction); // change action
+	server.on("/speed", HTTP_POST, handleSpeed);   // change speed
+	server.on("/gamma", HTTP_POST, handleGamma);   // change brightness
+	server.on("/config", HTTP_POST, handleConfig); // override everything with new config
 
 	server.begin();
 }
